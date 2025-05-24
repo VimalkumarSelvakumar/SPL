4d253ed92e3779355d6459e0645899c1e8ee5883
Subhasri Pandian <Subhasri.Pandian@analog.com>
2025-05-24 11:12:02 +0530

    feat: add logic to send data

---
+++ Gameleon/Gameleon/SendMessageData.cs
@@ -0,0 +1,35 @@
+﻿using Gameleon.Models;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gameleon
+{
+    public class SendMessageData
+    {
+        public static object Parse(byte[] message)
+        {
+            string json = JsonConvert.SerializeObject(message);
+
+            // Convert to UTF-8 bytes
+            byte[] jsonBytes = Encoding.UTF8.GetBytes(json);
+            int length = jsonBytes.Length;
+
+            // Prepare final buffer: 4 bytes length + json data
+            byte[] result = new byte[4 + length];
+
+            // Store length in first 4 bytes (big-endian)
+            result[0] = (byte)(length >> 24);
+            result[1] = (byte)(length >> 16);
+            result[2] = (byte)(length >> 8);
+            result[3] = (byte)(length);
+
+            Array.Copy(jsonBytes, 0, result, 4, length);
+
+            return result;
+        }
+    }
+}
